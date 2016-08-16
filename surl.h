f76f3c59b5ab348000a02c5d5e01fb5d3ccdaf1f
Tianyong Tang <tang3w@gmail.com>
2016-08-16 13:16:49 +0800

    Add shims for variadic function

--- surl.h
+++ surl.h
@@ -1,6 +1,64 @@
-#ifndef Surl_h
-#define Surl_h
+//
+//  surl.h
+//  Surl
+//
+//  Created by Tang Tianyong on 8/15/16.
+//  Copyright © 2016 LeanCloud. All rights reserved.
+//
+
+#ifndef surl_h
+#define surl_h
 
 #import <curl/curl.h>
 
-#endif
+#define CURL_INLINE extern inline
+
+typedef size_t (*curl_func)(char * ptr, size_t size, size_t num, void * ud);
+
+CURL_INLINE CURLcode curl_easy_setopt_string(CURL *curl, CURLoption option, const char *param) {
+    return curl_easy_setopt(curl, option, param);
+}
+
+CURL_INLINE CURLcode curl_easy_setopt_bool(CURL *curl, CURLoption option, bool param) {
+    return curl_easy_setopt(curl, option, param);
+}
+
+CURL_INLINE CURLcode curl_easy_setopt_func(CURL *handle, CURLoption option, curl_func param)
+{
+    return curl_easy_setopt(handle, option, param);
+}
+
+CURL_INLINE CURLcode curl_easy_setopt_pointer(CURL *handle, CURLoption option, const void* param)
+{
+    return curl_easy_setopt(handle, option, param);
+}
+
+CURL_INLINE CURLcode curl_easy_setopt_long(CURL *curl, CURLoption option, long param) {
+    return curl_easy_setopt(curl, option, param);
+}
+
+CURL_INLINE CURLcode curl_easy_setopt_slist(CURL *curl, CURLoption option, struct curl_slist *param) {
+    return curl_easy_setopt(curl, option, param);
+}
+
+CURL_INLINE CURLcode curl_easy_getinfo_long(CURL *handle, CURLINFO option, long *param)
+{
+    return curl_easy_getinfo(handle, option, param);
+}
+
+CURL_INLINE CURLcode curl_easy_getinfo_string(CURL *handle, CURLINFO option, const char **param)
+{
+    return curl_easy_getinfo(handle, option, param);
+}
+
+CURL_INLINE CURLcode curl_easy_getinfo_double(CURL *handle, CURLINFO option, double *param)
+{
+    return curl_easy_getinfo(handle, option, param);
+}
+
+CURL_INLINE CURLcode curl_easy_getinfo_slist(CURL *handle, CURLINFO option, struct curl_slist **param)
+{
+    return curl_easy_getinfo(handle, option, param);
+}
+
+#endif /* surl_h */
